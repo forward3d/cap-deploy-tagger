5647339a86b731e2f377ab0a646463649e992a40
Isaac Betesh <iybetesh@gmail.com>
2014-02-05 15:56:16 -0500

    latest_revision is based on the git server branch, not the deployment machine's branch

--- lib/cap-deploy-tagger.rb
+++ lib/cap-deploy-tagger.rb
@@ -5,21 +5,4 @@ require 'capistrano/deploy'
 load File.expand_path("../cap-deploy-tagger/tasks/tagger.rake", __FILE__)
 
 module CapDeployTagger
-  class Helper
-    
-    def self.tag(tag, stage)
-      git "tag -f #{tag}_#{stage}"
-      git "push -f --tags"
-      "#{tag}_#{stage}"
-    end
-    
-    def self.git(cmd)
-      `git #{cmd} 2>&1`.chomp
-    end
-    
-    def self.latest_revision
-      git "rev-parse --short HEAD"
-    end
-    
-  end
 end
